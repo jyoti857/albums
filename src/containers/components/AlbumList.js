7f2cbe807681f18e952417498aaa010673b25965
Jyoti RS <jyotiranjans@cnxsi.com>
2019-11-12 00:01:11 +0530

    some how fixed the build error

---
+++ src/containers/components/AlbumList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {View, Text} from 'react-native';
+
+const AlbumList = () => {
+  return (
+    <View>
+      <Text>This is Album list </Text>
+    </View>
+  );
+};
+
+export default AlbumList;
