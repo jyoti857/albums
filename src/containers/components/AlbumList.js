d07e630c1670a7a32b0566deea378d62f379da6d
Jyoti RS <jyotiranjans@cnxsi.com>
2019-11-12 00:43:47 +0530

    albums list came up

--- src/containers/components/AlbumList.js
+++ src/containers/components/AlbumList.js
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import request from '../../utils/request';
 
-import {View, Text} from 'react-native';
+import {View, Text, Image} from 'react-native';
+// import {TouchableRipple} from 'react-native-paper';
 
 const AlbumList = () => {
+  const [albumList, setAlbumList] = useState([]);
+  const url = 'http://rallycoding.herokuapp.com/api/music_albums/';
+  const options = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  useEffect(() => {
+    // const response = request(url, options);
+    // console.log('response ----> ', response);
+    fetch(url)
+      .then(data => data.json())
+      .then(data => {
+        console.log('data ==>', data, albumList);
+        setAlbumList(data);
+      })
+      .catch(err => {
+        console.log('error -->', err.json());
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <View>
       <Text>This is Album list </Text>
+      {albumList.map((album, index) => {
+        return (
+          <View key={index}>
+            <Text>{album.artist}</Text>
+            <Image
+              style={{width: 100, height: 100, borderRadius: 20}}
+              source={{url: album.thumbnail_image}}
+            />
+            <Text>{album.url}</Text>
+          </View>
+        );
+      })}
     </View>
   );
 };
--- src/containers/index.js
+++ src/containers/index.js
@@ -4,6 +4,7 @@ import {DynamicModuleLoader} from 'redux-dynamic-modules';
 import {Text} from 'react-native';
 import Headers from './components/Headers';
 import {getAlbumModule} from '../module';
+import AlbumList from './components/AlbumList';
 
 class Albums extends React.Component {
   render() {
@@ -12,6 +13,7 @@ class Albums extends React.Component {
         <Headers>
           <Text>Albums</Text>
         </Headers>
+        <AlbumList />
       </DynamicModuleLoader>
     );
   }
