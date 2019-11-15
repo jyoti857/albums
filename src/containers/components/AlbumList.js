e72d9d79ba8da6fdc9cc7c04fd5922017899b251
Jyoti RS <jyotiranjans@cnxsi.com>
2019-11-15 22:14:57 +0530

    album buy now button presentation only

--- src/containers/components/AlbumList.js
+++ src/containers/components/AlbumList.js
@@ -1,8 +1,18 @@
 import React, {useState, useEffect} from 'react';
 import request from '../../utils/request';
 
-import {View, Text, Image} from 'react-native';
+import {
+  View,
+  Text,
+  Image,
+  FlatList,
+  ScrollView,
+  StyleSheet,
+} from 'react-native';
 import {TouchableRipple} from 'react-native-paper';
+import Card_ from './Card_';
+import CardSection from './CardSection';
+import Button from '../../Components/Button';
 
 const AlbumList = () => {
   const [albumList, setAlbumList] = useState([]);
@@ -24,27 +34,65 @@ const AlbumList = () => {
         setAlbumList(data);
       })
       .catch(err => {
-        console.log('error -->', err.json());
+        console.log('error -->', err);
       });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return (
-    <View>
-      <Text>This is Album list </Text>
-      {albumList.map((album, index) => {
-        return (
-          <View key={index}>
-            <Text>{album.artist}</Text>
-            <Image
-              style={{width: 100, height: 100, borderRadius: 20}}
-              source={{url: album.thumbnail_image}}
-            />
-            <Text>{album.url}</Text>
-          </View>
-        );
-      })}
+    <View style={styles.container}>
+      <FlatList
+        data={albumList}
+        renderItem={({item}) => {
+          return (
+            <ScrollView>
+              <Card_>
+                <View style={{alignItems: 'center'}}>
+                  <CardSection>
+                    <Text>{item.artist}</Text>
+                  </CardSection>
+                </View>
+                <CardSection>
+                  <Image
+                    style={{width: 100, height: 100, borderRadius: 20}}
+                    source={{uri: item.thumbnail_image}}
+                  />
+                  <TouchableRipple
+                    onPress={() => console.log('Pressed')}
+                    rippleColor="transparent">
+                    <Text style={{color: 'black', fontSize: 30, padding: 20}}>
+                      {item.title}
+                    </Text>
+                  </TouchableRipple>
+                </CardSection>
+                <CardSection>
+                  <Image
+                    // eslint-disable-next-line react-native/no-inline-styles
+                    style={{
+                      height: 300,
+                      flex: 1,
+                      width: null,
+                      borderRadius: 20,
+                    }}
+                    source={{url: item.image}}
+                  />
+                </CardSection>
+                <CardSection>
+                  <Button title="Buy now" />
+                </CardSection>
+              </Card_>
+            </ScrollView>
+          );
+        }}
+        keyExtractor={item => item.url}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    marginBottom: 200,
+  },
+});
 
 export default AlbumList;
--- src/containers/index.js
+++ src/containers/index.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import {DynamicModuleLoader} from 'redux-dynamic-modules';
 
-import {Text} from 'react-native';
+import {Text, View} from 'react-native';
 import Headers from './components/Headers';
 import {getAlbumModule} from '../module';
 import AlbumList from './components/AlbumList';
 
+// to check the button component
+import Button from '../Components/Button';
+
 class Albums extends React.Component {
   render() {
     return (
+      //   <View style={{flex: 1}}>
       <DynamicModuleLoader modules={[getAlbumModule()]}>
         <Headers>
           <Text>Albums</Text>
         </Headers>
+        {/* <Button title="this is button title" /> */}
         <AlbumList />
       </DynamicModuleLoader>
+      //   </View>
     );
   }
 }
